6b2a9ec55abd46c6f475b87d01715173b6649964
Jesús Acuña <34004828+ManuelMaciel@users.noreply.github.com>
2022-08-16 16:45:44 -0400

    create mocks products

---
+++ backend/src/mocks.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Product 1',
+        description: 'Description 1'
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        description: 'Description 2'
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        description: 'Description 3'
+    }
+];
