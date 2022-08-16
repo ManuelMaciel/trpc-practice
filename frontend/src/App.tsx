ca45598be98621720b226d296fbbe3b067c6012f
Jesús Acuña <34004828+ManuelMaciel@users.noreply.github.com>
2022-08-16 16:22:04 -0400

    create connection with query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { trpc } from "./config/trpc";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() => trpc.createClient({
+    url: "http://localhost:3000/trpc",
+  }))
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <AppContent />
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
 }
 
-export default App
+const AppContent = () => {
+
+  const helloMessage = trpc.useQuery(['hello'])
+  console.log(helloMessage)
+  return (
+    <div>
+      <h1>{helloMessage.data}</h1>
+    </div>
+  );
+}
+
+export default App;
