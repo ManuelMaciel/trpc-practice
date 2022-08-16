fcbb9aca486c634db40de4be20bd1cee122637bb
Jesús Acuña <34004828+ManuelMaciel@users.noreply.github.com>
2022-08-16 16:45:21 -0400

    create mutation and validate input

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,30 @@
 import express from 'express';
 import cors from 'cors';
 import * as trpc from '@trpc/server';
-import * as trpcExpress from '@trpc/server/adapters/express'
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { z } from 'zod';
+
+import { products } from './mocks';
 
 const app = express();
 
-const appRouter = trpc.router().query('hello', {
-    resolve: () => {
-        return 'hello';
-    }
-})
+const appRouter = trpc.router()
+    .query('hello', {
+        resolve: () => {
+            return 'hello';
+        }
+    })
+    .query('getProducts', {
+        resolve: () => {
+            return products;
+        }
+    })
+    .mutation('createProduct', {
+        input: z.string(),
+        resolve: ({ input }) => {
+            return `${input} created`;
+        }
+    })
 
 export type AppRouter = typeof appRouter;
 
